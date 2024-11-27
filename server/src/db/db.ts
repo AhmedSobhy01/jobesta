f8da2741c8db9a42faec585eb0875003a7b5c021
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-11-27 13:15:43 +0200

    feat: add docker for server and db

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -6,7 +6,7 @@ configDotenv();
 
 // Create a new PostgreSQL client using environment variables
 const client = new pg.Client({
-  host: process.env.DB_HOST, // Database host
+  host: process.env.DB_HOST || 'localhost', // Database host
   port: parseInt(process.env.DB_PORT || '5432'), // Database port, default to 5432 if not provided
   user: process.env.DB_USER, // Database user
   password: process.env.DB_PASSWORD, // Database password
