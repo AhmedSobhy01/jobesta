a3899b425addeb60eda37ce5d96c9d09c3780e1f
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-11-30 02:07:43 +0200

    fix: updat skills with the previous work

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import db from '../db/db.js';
-import { updateFreelancer } from '../db/dbHelper.js';
+import { IPreviousWork } from '../models/model.js';
 
 export async function getUser(req: Request, res: Response) {
   if (!req.user) {
@@ -17,19 +17,64 @@ export async function getUser(req: Request, res: Response) {
     return;
   }
 
-  res.status(200).json({ message: 'User Found', data: query.rows[0] });
+  const userData = query.rows[0];
+  delete userData.password;
+  delete userData.id;
+
+  res.status(200).json({ message: 'User Found', data: userData });
 }
 
 export async function updateUser(req: Request, res: Response) {
   if (!req.user) {
-    res.status(401).json({ message: 'User not found' });
+    res.status(422).json({ message: 'User not found' });
     return;
   }
-  const update = await updateFreelancer({ id: req.user.id, ...req.body });
-  if(update){
-    res.status(201).json({message:"Updated freelancer"});
-    return;  
+
+  const id = req.user.id;
+  const bio = req.body.bio;
+
+  const user = await db.query(
+    'SELECT * FROM freelancers WHERE account_id = $1',
+    [id],
+  );
+
+  const updateFreelancerQuery = {
+    text: '',
+    values: [''],
+  };
+
+  if (user.rowCount !== null && user.rowCount > 0) {
+    updateFreelancerQuery.text = 'UPDATE freelancers SET bio = $1 RETURNING id';
+    updateFreelancerQuery.values = [bio];
+  } else {
+    updateFreelancerQuery.text =
+      'INSERT INTO freelancers (bio,balance,account_id) values ($1,$2,$3) RETURNING id';
+    updateFreelancerQuery.values = [bio, 0, id];
   }
-  res.status(400).json({message:"Error updating freelancer"});
-  return;
+
+  const previousWork = req.body.previousWork;
+  const skills = req.body.skills;
+  try {
+    const freelancerIdQuery = await db.query(updateFreelancerQuery);
+    for (const work of previousWork as Array<IPreviousWork>) {
+      await db.query('INSERT INTO previous_works values ($1,$2,$3,$4,$5)', [
+        freelancerIdQuery.rows[0].id,
+        work.order,
+        work.title,
+        work.description,
+        work.url || null,
+      ]);
+    }
+
+    for (const skill of skills) {
+      await db.query('INSERT INTO skills values ($1,$2)', [
+        freelancerIdQuery.rows[0].id,
+        skill,
+      ]);
+    }
+
+    res.status(201).json({ message: 'Updated freelancer' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating freelancer', error: err });
+  }
 }
---
+++ server/src/models/model.d.ts
@@ -0,0 +1,7 @@
+export interface IPreviousWork {
+  freelancer_id: string;
+  order: number;
+  title: string;
+  description: number;
+  url?: string;
+}
