f16a9b9e6a9dd1020b8da89000be05e08eccb25b
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-11-30 17:20:40 +0200

    fix: use freelaner_id instead of account_id

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -11,7 +11,7 @@ export async function updateUser(req: Request, res: Response) {
   const id = req.user!.id;
   const bio = req.body.bio;
 
-  const user = await db.query(
+  const freelancerAccount = await db.query(
     'SELECT * FROM freelancers WHERE account_id = $1',
     [id],
   );
@@ -21,27 +21,37 @@ export async function updateUser(req: Request, res: Response) {
     values: [''],
   };
 
-  if (user.rowCount !== null && user.rowCount > 0) {
-    updateFreelancerQuery.text = 'UPDATE freelancers SET bio = $1 RETURNING id';
-    updateFreelancerQuery.values = [bio];
+  if (freelancerAccount.rowCount !== null && freelancerAccount.rowCount > 0) {
+    updateFreelancerQuery.text =
+      'UPDATE freelancers SET bio = $1 WHERE account_id = $2 RETURNING id';
+    updateFreelancerQuery.values = [bio, id];
   } else {
     updateFreelancerQuery.text =
       'INSERT INTO freelancers (bio,balance,account_id) values ($1,$2,$3) RETURNING id';
     updateFreelancerQuery.values = [bio, 0, id];
   }
 
-  const previousWork = req.body.previousWork;
+  const previousWork = req.body.previousWork as Array<IPreviousWork>;
   const skills = req.body.skills;
   try {
     const freelancerIdQuery = await db.query(updateFreelancerQuery);
+    const { id: freelacnerId } = freelancerIdQuery.rows[0];
 
-    await db.query('DELETE FROM skills WHERE freelancer_id = $1', [id]);
-    await db.query('DELETE FROM previous_works WHERE account_id = $1', [id]);
+    await db.query('DELETE FROM skills WHERE freelancer_id = $1', [
+      freelacnerId,
+    ]);
+    await db.query('DELETE FROM previous_works WHERE freelancer_id = $1', [
+      freelacnerId,
+    ]);
 
-    for (const work of previousWork as Array<IPreviousWork>) {
+    previousWork.sort(
+      (a: IPreviousWork, b: IPreviousWork) => a.order - b.order,
+    );
+
+    for (const [i, work] of previousWork.entries()) {
       await db.query('INSERT INTO previous_works values ($1,$2,$3,$4,$5)', [
-        freelancerIdQuery.rows[0].id,
-        work.order,
+        freelacnerId,
+        i + 1,
         work.title,
         work.description,
         work.url || null,
@@ -50,7 +60,7 @@ export async function updateUser(req: Request, res: Response) {
 
     for (const skill of skills) {
       await db.query('INSERT INTO skills values ($1,$2)', [
-        freelancerIdQuery.rows[0].id,
+        freelacnerId,
         skill,
       ]);
     }
