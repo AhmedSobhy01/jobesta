28a89c99e15fb9ddb53127de1301bee929bb2c1e
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-11-30 16:42:19 +0200

    refactor: remove unnecessary blank line in usersRoutes

--- server/src/routes/usersRoutes.ts
+++ server/src/routes/usersRoutes.ts
@@ -2,7 +2,6 @@ import router from 'express';
 import { getUser, updateUser } from '../controllers/usersController.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
-
 const usersRouter = router();
 
 usersRouter.get('/me', authenticate, getUser);
