31b0e9642ec5309a620245d829459e6460be0190
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-12-05 23:39:21 +0200

    fix: adjust CSS classes for alignment and whitespace in NavBar components

--- client/src/components/NavBar/NavBarItem.tsx
+++ client/src/components/NavBar/NavBarItem.tsx
@@ -5,10 +5,10 @@ const NavBarItem: React.FC<{ page: string; children: React.ReactNode }> = (
   props,
 ) => {
   const activeCss =
-    'block w-full text-left px-4 py-2 text-sm text-green-700 md:bg-transparent ';
+    'block w-full text-center px-4 py-2 text-sm text-green-700 md:bg-transparent whitespace-nowrap';
 
   const notActiveCss =
-    'block w-full text-left px-4 py-2 text-sm text-gray-700 hover:text-green-700';
+    'block w-full text-center px-4 py-2 text-sm text-gray-200 hover:text-green-700 whitespace-nowrap';
 
   return (
     <>
