6caecd5f42d2dec39645cfaddfbcb203c1aee750
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-06 23:58:34 +0200

    feat(auth, jobs): improve error handling and enhance job retrieval with detailed client and category information

--- server/src/controllers/jobController.ts
+++ server/src/controllers/jobController.ts
@@ -15,24 +15,26 @@ export async function createJob(req: Request, res: Response): Promise<void> {
 }
 
 export async function getJobs(req: Request, res: Response): Promise<void> {
-  let queryString = "SELECT * FROM jobs WHERE status = 'open' ";
-  try {
-    if (req.query.category) {
-      queryString += `AND category_id = ${req.query.category} `;
-    }
-    if (req.query.minBudget) {
-      queryString += `AND budget >=  ${req.query.minBudget} `;
-    }
-    if (req.query.maxBudget) {
-      queryString += `AND budget <=  ${req.query.maxBudget} `;
-    }
-    if (req.query.sortBy) {
-      queryString += `ORDER BY ${req.query.sortBy} `;
-      if (req.query.sortOrder) {
-        queryString += `${req.query.sortOrder} `;
-      }
-    }
+  let queryString =
+    "SELECT j.id, j.status, j.budget, j.duration, j.title, j.description, j.created_at, client.first_name, client.last_name, client.username, client.profile_picture,c.id category_id,c.name ,c.description category_description FROM jobs j JOIN categories c ON c.id = j.category_id JOIN accounts client ON client.id = j.client_id WHERE status = 'open' ";
 
+  if (req.query.category) {
+    queryString += `AND category_id = ${req.query.category} `;
+  }
+  if (req.query.minBudget) {
+    queryString += `AND budget >=  ${req.query.minBudget} `;
+  }
+  if (req.query.maxBudget) {
+    queryString += `AND budget <=  ${req.query.maxBudget} `;
+  }
+  if (req.query.sortBy) {
+    queryString += `ORDER BY ${req.query.sortBy} `;
+    if (req.query.sortOrder) {
+      queryString += `${req.query.sortOrder} `;
+    }
+  }
+
+  try {
     const jobsQuery = await db.query(queryString);
 
     const jobs = jobsQuery.rows.map((job) => {
@@ -43,9 +45,18 @@ export async function getJobs(req: Request, res: Response): Promise<void> {
         duration: job.duration,
         title: job.title,
         description: job.description,
-        categoryId: job.category_id,
+        category: {
+          id: job.category_id,
+          name: job.name,
+          description: job.category_description,
+        },
         createAt: job.created_at,
-        clientId: job.client_id,
+        client: {
+          first_name: job.first_name,
+          last_name: job.last_name,
+          username: job.username,
+          profile_picture: job.profile_picture,
+        },
       };
     });
 
@@ -56,26 +67,47 @@ export async function getJobs(req: Request, res: Response): Promise<void> {
         jobs,
       },
     });
-  } catch (err) {
-    console.log(err);
+  } catch {
     res.status(500).json({ status: false, message: 'Error retrieving jobs' });
   }
 }
 
 export async function getJobById(req: Request, res: Response) {
   try {
-    const jobQuery = await db.query('SELECT * FROM jobs WHERE id = $1', [
-      req.params.id,
-    ]);
+    const jobQuery = await db.query(
+      'SELECT j.id, j.status, j.budget, j.duration, j.title, j.description, j.created_at, client.first_name, client.last_name, client.username, client.profile_picture,c.id category_id,c.name ,c.description category_description FROM jobs j JOIN categories c ON c.id = j.category_id JOIN accounts client ON client.id = j.client_id WHERE j.id = $1 ',
+      [req.params.id],
+    );
     if (jobQuery.rows.length === 0) {
       res.status(404).json({ status: false, message: 'Job not found' });
       return;
     }
+    const job = {
+      id: jobQuery.rows[0].id,
+      status: jobQuery.rows[0].status,
+      budget: jobQuery.rows[0].budget,
+      duration: jobQuery.rows[0].duration,
+      title: jobQuery.rows[0].title,
+      description: jobQuery.rows[0].description,
+      category: {
+        id: jobQuery.rows[0].category_id,
+        name: jobQuery.rows[0].name,
+        description: jobQuery.rows[0].category_description,
+      },
+      createAt: jobQuery.rows[0].created_at,
+      client: {
+        first_name: jobQuery.rows[0].first_name,
+        last_name: jobQuery.rows[0].last_name,
+        username: jobQuery.rows[0].username,
+        profile_picture: jobQuery.rows[0].profile_picture,
+      },
+    };
+
     res.json({
       status: true,
       message: 'Job retrieved',
       data: {
-        job: jobQuery.rows[0],
+        job,
       },
     });
   } catch {
