77f2ebc06735615c41c22c1cfd29ac53fb6a980f
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-10 00:41:07 +0200

    chore: update format

--- server/src/routes/adminRoutes/badgesRoutes.ts
+++ server/src/routes/adminRoutes/badgesRoutes.ts
@@ -3,9 +3,7 @@ import {
   getBadges,
   deleteBadge,
 } from '../../controllers/adminControllers/badgesController.js';
-import {
-	  deleteBadgeValidationRules,
-} from '../../validations/adminValidations/badgesValidation.js';
+import { deleteBadgeValidationRules } from '../../validations/adminValidations/badgesValidation.js';
 import { validateRequest } from '../../middlewares/validationMiddleware.js';
 const badgesRouter = router();
 
