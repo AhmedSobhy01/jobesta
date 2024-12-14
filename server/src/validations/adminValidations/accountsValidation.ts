6576fbf8a384cdadfe558977cc962fd450e54939
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-15 01:04:20 +0200

    fix: validation works again

--- server/src/validations/adminValidations/accountsValidation.ts
+++ server/src/validations/adminValidations/accountsValidation.ts
@@ -9,6 +9,80 @@ export const getAccountsValidationRules = [
     .withMessage('Role must be either admin or freelancer or client'),
 ];
 
+export const freelancerDataValidationRules = [
+  body('bio')
+    .optional()
+    .trim()
+    .isString()
+    .withMessage('Bio must be a string')
+    .isLength({ max: 1023 })
+    .withMessage('Bio must not exceed 1023 characters'),
+
+  body('skills')
+    .optional()
+    .isArray()
+    .withMessage('Skills must be an array')
+    .custom((skills) => {
+      if (new Set(skills).size !== skills.length) {
+        throw new Error('Skills must be unique');
+      }
+
+      return true;
+    }),
+
+  body('skills.*')
+    .trim()
+    .isString()
+    .withMessage('Skill must be a string')
+    .notEmpty()
+    .withMessage('Skill must not be empty')
+    .isLength({ max: 255 })
+    .withMessage('Skill must not exceed 255 characters'),
+
+  body('previousWork')
+    .optional()
+    .isArray()
+    .withMessage('Previous work must be an array')
+    .custom((previousWork) => {
+      if (
+        new Set(previousWork.map((work: IPreviousWork) => work.order)).size !==
+        previousWork.length
+      ) {
+        throw new Error('Previous work order must be unique');
+      }
+
+      return true;
+    }),
+
+  body('previousWork.*.title')
+    .trim()
+    .notEmpty()
+    .withMessage('Title must not be empty')
+    .isString()
+    .withMessage('Title must be a string')
+    .isLength({ max: 255 })
+    .withMessage('Title must not exceed 255 characters'),
+
+  body('previousWork.*.description')
+    .trim()
+    .notEmpty()
+    .withMessage('Description must not be empty')
+    .isString()
+    .withMessage('Description must be a string')
+    .isLength({ max: 1000 })
+    .withMessage('Description must not exceed 1000 characters'),
+
+  body('previousWork.*.url')
+    .optional()
+    .trim()
+    .isURL()
+    .withMessage('URL must be a valid URL'),
+
+  body('previousWork.*.order')
+    .isInt({ min: 1 })
+    .withMessage('Order must be a positive integer'),
+];
+
 export const createAccountValidationRules = [
   body('firstName')
     .trim()
@@ -93,6 +167,7 @@ export const createAccountValidationRules = [
     .withMessage('Role is required')
     .isIn(['client', 'freelancer', 'admin'])
     .withMessage('Role must be either client or freelancer or admin'),
+  ...freelancerDataValidationRules,
 ];
 
 export const updateAccountValidationRules = [
@@ -174,6 +249,7 @@ export const updateAccountValidationRules = [
 
       return true;
     }),
+  ...freelancerDataValidationRules,
 ];
 
 export const deleteAccountValidationRules = [
@@ -251,80 +327,6 @@ export const unbanAccountValidationRules = [
 
       return true;
     }),
-];
-
-export const createOrUpdateFreelancerValidationRules = [
-  body('bio')
-    .optional()
-    .trim()
-    .isString()
-    .withMessage('Bio must be a string')
-    .isLength({ max: 1023 })
-    .withMessage('Bio must not exceed 1023 characters'),
-
-  body('skills')
-    .optional()
-    .isArray()
-    .withMessage('Skills must be an array')
-    .custom((skills) => {
-      if (new Set(skills).size !== skills.length) {
-        throw new Error('Skills must be unique');
-      }
-
-      return true;
-    }),
-
-  body('skills.*')
-    .trim()
-    .isString()
-    .withMessage('Skill must be a string')
-    .notEmpty()
-    .withMessage('Skill must not be empty')
-    .isLength({ max: 255 })
-    .withMessage('Skill must not exceed 255 characters'),
-
-  body('previousWorks')
-    .optional()
-    .isArray()
-    .withMessage('Previous work must be an array')
-    .custom((previousWork) => {
-      if (
-        new Set(previousWork.map((work: IPreviousWork) => work.order)).size !==
-        previousWork.length
-      ) {
-        throw new Error('Previous work order must be unique');
-      }
-
-      return true;
-    }),
-
-  body('previousWorks.*.title')
-    .trim()
-    .notEmpty()
-    .withMessage('Title must not be empty')
-    .isString()
-    .withMessage('Title must be a string')
-    .isLength({ max: 255 })
-    .withMessage('Title must not exceed 255 characters'),
-
-  body('previousWorks.*.description')
-    .trim()
-    .notEmpty()
-    .withMessage('Description must not be empty')
-    .isString()
-    .withMessage('Description must be a string')
-    .isLength({ max: 1000 })
-    .withMessage('Description must not exceed 1000 characters'),
-
-  body('previousWorks.*.url')
-    .optional()
-    .trim()
-    .isURL()
-    .withMessage('URL must be a valid URL'),
-
-  body('previousWorks.*.order')
-    .isInt({ min: 1 })
-    .withMessage('Order must be a positive integer'),
 ];
 
 export const getFreelancerValidationRules = [
