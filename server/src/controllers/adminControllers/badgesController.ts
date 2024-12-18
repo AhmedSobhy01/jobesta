89cab079e172eb2afe7c2a2ee3c50b459aa9cf30
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-18 03:47:00 +0200

    refactor: remove unneeded public folder

--- server/src/controllers/adminControllers/badgesController.ts
+++ server/src/controllers/adminControllers/badgesController.ts
@@ -4,11 +4,47 @@ import fs from 'fs/promises';
 
 export async function getBadges(req: Request, res: Response) {
   try {
-    const badges = await db.query('SELECT * FROM badges');
+    let badgesQueryString = 'SELECT * FROM badges';
+    const countQuery = 'SELECT COUNT(*) FROM badges';
+
+    const limit = parseInt(process.env.ADMIN_PAGINATION_LIMIT || '10');
+
+    const totalItemsQuery = await db.query(countQuery);
+    const totalItems = parseInt(totalItemsQuery.rows[0].count);
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const page =
+      parseInt(req.query.page as string) > 0 &&
+      parseInt(req.query.page as string) <= totalPages
+        ? parseInt(req.query.page as string)
+        : 1;
+    const offset = (page - 1) * limit;
+
+    badgesQueryString += ` LIMIT ${limit} OFFSET ${offset}`;
+
+    const result = await db.query(badgesQueryString);
+
+    const badges = result.rows.map((badge) => {
+      return {
+        id: badge.id,
+        name: badge.name,
+        description: badge.description,
+        icon: badge.icon,
+      };
+    });
+
     res.json({
       status: true,
       message: 'Badges fetched',
-      data: { badges: badges.rows },
+      data: {
+        badges,
+        pagination: {
+          currentPage: page,
+          totalItems,
+          totalPages,
+          perPage: limit,
+        },
+      },
     });
   } catch {
     res.status(500).json({
