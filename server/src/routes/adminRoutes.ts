29453ec8f05e96049df93f3acea5b48d59f0a708
Ahmed Amr <43810060+AhmedAmrNabil@users.noreply.github.com>
2024-12-19 01:48:12 +0200

    feat: admin dashboard statistics (#42)

--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -4,6 +4,7 @@ import { checkIfAdmin } from '../middlewares/adminMiddleware.js';
 import categoriesRouter from './adminRoutes/categoriesRoutes.js';
 import badgesRouter from './adminRoutes/badgesRoutes.js';
 import accountsRouter from './adminRoutes/accountsRoutes.js';
+import { getStatistics } from '../controllers/adminControllers/statisticsController.js';
 
 const adminRoutes = router();
 
@@ -12,5 +13,6 @@ adminRoutes.use(authenticate, checkIfAdmin);
 adminRoutes.use('/categories', categoriesRouter);
 adminRoutes.use('/badges', badgesRouter);
 adminRoutes.use('/accounts', accountsRouter);
+adminRoutes.get('/statistics', getStatistics);
 
 export default adminRoutes;
