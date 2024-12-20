7553dae12ccc405adf5ddfb39f038fdb7bd5bfc4
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-12-20 03:29:26 +0200

    feat: add admin withdrawal management functionality with routes, validations, and controller methods

--- server/src/controllers/adminControllers/withdrawalController.ts
+++ server/src/controllers/adminControllers/withdrawalController.ts
@@ -1,17 +1,33 @@
 import { Request, Response } from 'express';
-import db from '../db/db.js';
+import db from '../../db/db.js';
 
 export async function getWithdrawals(
   req: Request,
   res: Response,
 ): Promise<void> {
   try {
+    let queryString = `
+      SELECT 
+        w.*,
+        a.username AS freelancer_username,
+        a.first_name AS freelancer_first_name,
+        a.last_name AS freelancer_last_name
+      FROM withdrawals w 
+      JOIN freelancers f ON w.freelancer_id = f.id 
+      JOIN accounts a ON f.id = a.id
+    `;
+    let countString = 'SELECT COUNT(*) FROM withdrawals';
+    const queryParams = [];
+
+    if (req.query.status) {
+      queryString += ' WHERE status = $1';
+      countString += ' WHERE status = $1';
+      queryParams.push(req.query.status as string);
+    }
+
     const limit = parseInt(process.env.PAGINATION_LIMIT || '10');
 
-    const totalItemsQuery = await db.query(
-      'SELECT COUNT(*) FROM withdrawals WHERE freelancer_id = $1',
-      [req.user!.freelancer!.id],
-    );
+    const totalItemsQuery = await db.query(countString, queryParams);
     const totalItems = parseInt(totalItemsQuery.rows[0].count);
     const totalPages = Math.ceil(totalItems / limit);
 
@@ -22,10 +38,9 @@ export async function getWithdrawals(
         : 1;
     const offset = (page - 1) * limit;
 
-    const result = await db.query(
-      `SELECT * FROM withdrawals WHERE freelancer_id = $1 ORDER BY requested_at DESC LIMIT $2 OFFSET $3`,
-      [req.user!.freelancer!.id, limit, offset],
-    );
+    queryString += ` ORDER BY requested_at DESC LIMIT ${limit} OFFSET ${offset}`;
+
+    const result = await db.query(queryString, queryParams);
 
     const withdrawals = result.rows.map((withdrawal) => {
       return {
@@ -34,6 +49,11 @@ export async function getWithdrawals(
         amount: withdrawal.amount,
         paymentMethod: withdrawal.payment_method,
         paymentUsername: withdrawal.payment_username,
+        freelancer: {
+          username: withdrawal.freelancer_username,
+          firstName: withdrawal.freelancer_first_name,
+          lastName: withdrawal.freelancer_last_name,
+        },
         requestedAt: withdrawal.requested_at,
       };
     });
@@ -57,31 +77,24 @@ export async function getWithdrawals(
   }
 }
 
-export async function createWithdrawal(
+export async function completeWithdrawal(
   req: Request,
   res: Response,
 ): Promise<void> {
-  const { amount, paymentMethod, paymentUsername } = req.body;
-
   try {
-    await db.query(
-      'INSERT INTO withdrawals (amount, payment_method, payment_username, freelancer_id) VALUES ($1, $2, $3, $4)',
-      [amount, paymentMethod, paymentUsername, req.user!.freelancer!.id],
-    );
+    await db.query('UPDATE withdrawals SET status = $1 WHERE id = $2', [
+      'completed',
+      req.params.id,
+    ]);
 
-    await db.query(
-      'UPDATE freelancers SET balance = balance - $1 WHERE id = $2',
-      [amount, req.user!.freelancer!.id],
-    );
-
-    res.status(201).json({
+    res.status(200).json({
       status: true,
-      message: 'Withdrawal created',
+      message: 'Withdrawal completed',
     });
   } catch {
     res
       .status(500)
-      .json({ status: false, message: 'Error creating withdrawal' });
+      .json({ status: false, message: 'Error completing withdrawal' });
   }
 }
 
