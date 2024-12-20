1fdd8c54359b4b1031e89a74776849184b904500
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-12-20 13:19:42 +0200

    feat: add total unread notifications count to getNotifications response

--- server/src/controllers/notificationController.ts
+++ server/src/controllers/notificationController.ts
@@ -5,27 +5,27 @@ export async function getNotifications(
   req: Request,
   res: Response,
 ): Promise<void> {
-  const account_id = req.user!.id;
-  let queryString = `SELECT n.id, n.type, n.url, n.message, n.is_read, n.created_at FROM notifications n WHERE account_id = ${account_id}`;
-
-  const countQuery = `SELECT COUNT(*) FROM notifications n WHERE account_id = ${account_id}`;
-
-  const limit = parseInt(process.env.PAGINATION_LIMIT || '10');
-
-  const totalItemsQuery = await db.query(countQuery);
-  const totalItems = parseInt(totalItemsQuery.rows[0].count);
-  const totalPages = Math.ceil(totalItems / limit);
-
-  const page =
-    parseInt(req.query.page as string) > 0 &&
-    parseInt(req.query.page as string) <= totalPages
-      ? parseInt(req.query.page as string)
-      : 1;
-  const offset = (page - 1) * limit;
-
-  queryString += ` LIMIT ${limit} OFFSET ${offset}`;
-
   try {
+    const account_id = req.user!.id;
+    let queryString = `SELECT n.id, n.type, n.url, n.message, n.is_read, n.created_at FROM notifications n WHERE account_id = ${account_id} ORDER BY created_at DESC`;
+
+    const countQuery = `SELECT COUNT(*) FROM notifications n WHERE account_id = ${account_id}`;
+
+    const limit = parseInt(process.env.PAGINATION_LIMIT || '10');
+
+    const totalItemsQuery = await db.query(countQuery);
+    const totalItems = parseInt(totalItemsQuery.rows[0].count);
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const page =
+      parseInt(req.query.page as string) > 0 &&
+      parseInt(req.query.page as string) <= totalPages
+        ? parseInt(req.query.page as string)
+        : 1;
+    const offset = (page - 1) * limit;
+
+    queryString += ` LIMIT ${limit} OFFSET ${offset}`;
+
     const notificationsQuery = await db.query(queryString);
 
     const notifications = notificationsQuery.rows.map((notification) => {
@@ -38,11 +38,16 @@ export async function getNotifications(
       };
     });
 
+    const totalUnreadQuery = await db.query(
+      `SELECT COUNT(*) FROM notifications n WHERE account_id = ${account_id} AND is_read = false`,
+    );
+
     res.json({
       status: true,
       message: 'Notifications retrieved',
       data: {
         notifications,
+        totalUnread: parseInt(totalUnreadQuery.rows[0].count),
         pagination: {
           currentPage: page,
           totalItems,
