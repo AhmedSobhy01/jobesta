dd4b574e65c8b2f08777f3421488649f4cd4a03f
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-21 04:08:54 +0200

    feat: update client create job modal

--- client/src/components/Jobs/EditJobModal.tsx
+++ client/src/components/Jobs/EditJobModal.tsx
@@ -122,113 +122,127 @@ const EditJobModal = ({ job, onClose }: { job: Job; onClose: () => void }) => {
       className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
       onMouseDown={handleModalClick}
     >
-      <div className="bg-white rounded-lg shadow-lg w-full max-w-lg p-6 relative">
-        <button
-          onClick={handleClose}
-          className="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+      <div className="flex flex-col bg-white rounded-lg shadow-lg max-w-lg w-full p-6 relative max-h-[90vh]">
+        <div>
+          <button
+            className="absolute top-6 right-6 text-gray-400 hover:text-gray-800 focus:outline-none p-0 m-0"
+            onClick={handleClose}
+          >
+            <FontAwesomeIcon icon={faXmark} className="text-xl" />
+          </button>
+          <h2 className="text-xl font-bold text-gray-800 mb-4 text-center">
+            Edit job
+          </h2>
+        </div>
+
+        <form
+          onSubmit={handleUpdateJob}
+          className="space-y-4 flex flex-col max-h-[70vh]"
         >
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-        <h2 className="text-2xl font-bold mb-4 text-gray-800">Edit Job</h2>
-
-        <form onSubmit={handleUpdateJob} className="space-y-4">
-          <div>
-            <label className="block text-gray-700 font-medium mb-2">
-              Title
-            </label>
-            <input
-              type="text"
-              name="title"
-              defaultValue={job.title}
-              placeholder="Enter job title"
-              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
-              required
-            />
-            <p className="text-sm text-red-500 mt-1">{errors?.title}</p>
-          </div>
-
-          <div>
-            <label className="block text-gray-700 font-medium mb-2">
-              Description
-            </label>
-            <textarea
-              name="description"
-              defaultValue={job.description}
-              placeholder="Enter job description"
-              rows={4}
-              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
-              required
-            ></textarea>
-            <p className="text-sm text-red-500 mt-1">{errors?.description}</p>
-          </div>
-
-          <div>
-            <label className="block text-gray-700 font-medium mb-2">
-              Category
-            </label>
-            {loading ? (
-              <SkeletonLoader />
-            ) : (
-              <select
-                name="category"
-                defaultValue={job.category.id}
+          <div className=" overflow-y-auto">
+            <div className="mb-4 mx-2">
+              <label className="block text-gray-700 font-medium mb-2">
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                defaultValue={job.title}
+                placeholder="Enter job title"
                 className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
                 required
-              >
-                {categories.map((category: JobCategory) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-            )}
-            <p className="text-sm text-red-500 mt-1">{errors?.category}</p>
-          </div>
+              />
+              <p className="text-sm text-red-500 mt-1">{errors?.title}</p>
+            </div>
 
-          <div>
-            <label className="block text-gray-700 font-medium mb-2">
-              Budget ($)
-            </label>
-            <input
-              type="number"
-              name="budget"
-              defaultValue={job.budget}
-              placeholder="Enter budget"
-              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
-              required
-              min={1}
-              step={0.01}
-            />
-            <p className="text-sm text-red-500 mt-1">{errors?.budget}</p>
-          </div>
+            <div className="mb-4 mx-2">
+              <label className="block text-gray-700 font-medium mb-2">
+                Description
+              </label>
+              <textarea
+                name="description"
+                defaultValue={job.description}
+                placeholder="Enter job description"
+                rows={4}
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
+                required
+              ></textarea>
+              <p className="text-sm text-red-500 mt-1">{errors?.description}</p>
+            </div>
 
-          <div>
-            <label className="block text-gray-700 font-medium mb-2">
-              Duration (days)
-            </label>
-            <input
-              type="number"
-              name="duration"
-              defaultValue={job.duration}
-              placeholder="Enter duration"
-              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
-              required
-              min={1}
-            />
-            <p className="text-sm text-red-500 mt-1">{errors?.duration}</p>
-          </div>
+            <div className="mb-4 mx-2">
+              <label className="block text-gray-700 font-medium mb-2">
+                Category
+              </label>
+              {loading ? (
+                <SkeletonLoader />
+              ) : (
+                <select
+                  name="category"
+                  defaultValue={job.category.id}
+                  className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
+                  required
+                >
+                  {categories.map((category: JobCategory) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  ))}
+                </select>
+              )}
+              <p className="text-sm text-red-500 mt-1">{errors?.category}</p>
+            </div>
 
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className={`w-full py-2 bg-emerald-600  text-white rounded-lg transition-all ${
-              isSubmitting
-                ? 'cursor-not-allowed bg-opacity-50'
-                : 'hover:bg-emerald-700'
-            }`}
-          >
-            {isSubmitting ? 'Updating...' : 'Update Job'}
-          </button>
+            <div className="mb-4 mx-2">
+              <label className="block text-gray-700 font-medium mb-2">
+                Budget ($)
+              </label>
+              <input
+                type="number"
+                name="budget"
+                defaultValue={job.budget}
+                placeholder="Enter budget"
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
+                required
+                min={1}
+                step={0.01}
+              />
+              <p className="text-sm text-red-500 mt-1">{errors?.budget}</p>
+            </div>
+
+            <div className="mb-4 mx-2">
+              <label className="block text-gray-700 font-medium mb-2">
+                Duration (days)
+              </label>
+              <input
+                type="number"
+                name="duration"
+                defaultValue={job.duration}
+                placeholder="Enter duration"
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 focus:outline-none"
+                required
+                min={1}
+              />
+              <p className="text-sm text-red-500 mt-1">{errors?.duration}</p>
+            </div>
+          </div>
+          <div className="mt-6 flex justify-end space-x-3">
+            <button
+              onClick={handleClose}
+              className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 focus:outline-none"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="submit"
+              className={`px-4 py-2 rounded-md focus:outline-none  bg-emerald-500
+            ${isSubmitting ? 'bg-opacity-50 cursor-not-allowed' : 'text-white hover:bg-emerald-600'}`}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'Updating...' : 'Update job'}
+            </button>
+          </div>
         </form>
       </div>
     </div>,
