ab78f6161909826f78e0d6e79b191071823631ac
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-26 05:56:03 +0200

    fix: fix imports

--- server/src/sockets/socket.ts
+++ server/src/sockets/socket.ts
@@ -1,8 +1,8 @@
 import { Server } from 'socket.io';
-import { authenticateSocket } from '../middlewares/authMiddleware';
+import { authenticateSocket } from '../middlewares/authMiddleware.js';
 
-import { chatHandler } from './chatHandler';
-import { notificationsHandler } from './notificationsHandler';
+import { chatHandler } from './chatHandler.js';
+import { notificationsHandler } from './notificationsHandler.js';
 
 export function setupSocketHandlers(io: Server) {
   io.use(authenticateSocket);
